7e91db0e50f359b1eadbc6328f7eba8498ec13f2
mattwvu <53435971+mattwvu@users.noreply.github.com>
2022-10-28 13:12:51 -0400

    Add files via upload

---
+++ keys_workshopfile.py
@@ -0,0 +1,8 @@
+
+consumer_key = 'YourConsumerKey'
+consumer_secret = 'YourConsumerSecret'
+access_token = 'YourAccessToken'
+access_token_secret = 'YourAccessTokenSecret'
+bearer_token = 'YourBearerToken'
+
+mapquest_key = 'YourAPIKey'
